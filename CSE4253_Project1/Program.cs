cbbf6424a16f852bd539de9260b29a5c0bd50c63
John Choi <choi.1655@osu.edu>
2020-09-16 00:11:55 -0400

    Simon's stuff idk

--- CSE4253_Project1/Program.cs
+++ CSE4253_Project1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections.Generic;
 
 namespace CSE4253_Project1
 {
@@ -6,7 +9,60 @@ namespace CSE4253_Project1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter Something");
+
+            var result = new List<char>();
+
+            int verticalP = 0;
+            int horizontalP = 0;
+
+            while (true)
+            {
+                var input = Console.ReadKey(true);
+
+                if (input.Modifiers.HasFlag(ConsoleModifiers.Control) && input.Key.HasFlag(ConsoleKey.Enter))
+                {
+                    break;
+                }
+
+                if (char.IsLetterOrDigit(input.KeyChar))
+                {
+                    if (verticalP == result.Count)
+                    {
+                        result.Insert(verticalP++, input.KeyChar);
+                    }
+                    else
+                    {
+                        result[verticalP] = input.KeyChar;
+                    }
+
+                }
+                else if (input.Key == ConsoleKey.LeftArrow && verticalP > 0)
+                { 
+                    verticalP--;
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                }
+                else if (input.Key == ConsoleKey.RightArrow && verticalP < int.MaxValue)
+                {
+                    verticalP++;
+                    Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                }
+                else if (input.Key == ConsoleKey.UpArrow && horizontalP < int.MaxValue)
+                {
+                    horizontalP++;
+                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+                }
+                else if (input.Key == ConsoleKey.DownArrow && horizontalP > 0)
+                {
+                    horizontalP--;
+                    Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                }
+
+
+
+            }
+
+
         }
     }
 }
